86c250f7ba6dbd2a279d2a184b2fc16fef35cad6
matth <mtjcai@outlook.com>
2022-05-02 16:25:56 -0400

    deploy of web

--- startup.ps1
+++ startup.ps1
@@ -1,2 +1,2 @@
-$Env.mongodb_URI = 'mongodb://localhost:27017/cs103a_L36'
+$Env.mongodb_URI = 'mongodb+srv://cluster0:ODFXoTWeUGq4BtYG@cluster0.ufrdt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 npm start
--- startup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export mongodb_URI='mongodb://localhost:27017/cs103a_L36'
-echo "connecting to $mongodb_URI"
-nodemon
